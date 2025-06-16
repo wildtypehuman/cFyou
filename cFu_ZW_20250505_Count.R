7461f50b1559b9e55e78e93d06647d9010928ada
wildtypehuman <alina.levin@ucsf.du>
2025-06-16 10:11:16 -0700

    Weihao zw134 and 135 Count data processed with added column AlinaDilution

--- cFu_ZW_20250505_Count.R
+++ cFu_ZW_20250505_Count.R
@@ -24,6 +24,36 @@ cFu_ZW_20250505_Count <- datzw134C %>%
 
 #Add metadata
 
+cFu_ZW_20250505_Count <- cFu_ZW_20250505_Count %>%
+  mutate(
+    Experiment_ID = "zw134",
+    Collected_By = "WZ",
+    Alina_Dilution = "None",
+    Date_Plated = "Monday, May 5, 2025",
+    Date_Read = "Friday, May 30, 2025",
+    MTB_Strain_Type = "Rv/pMv261-zsGreen",
+    Strain_Modifications = NA,
+    Infection_Route = "Aerosol",
+    Mouse_Type = "B6",
+    Treatment = NA,
+    Organ_Tissue = "Spleen",
+    Sample_Storage = "Frozen after CFU",
+    Preprocessing = "Mash in 6mL PBST",
+    Tissue_Homogenization_Method = "Mash in 6mL PBST",
+    Timepoint_dpi = 28,
+    Log_Dilution_Factor = "-1, -2, -3, -4",
+    Media = "7H11",
+    Media_Additive = "none",
+    Plate_Type = "12 well",
+    Spreading_Method = NA,
+    Total_Plates = NA,
+    Number_of_Replicates = 3,
+    Volume_Per_Plate_uL = 50
+  )
+
+#export final CFU
+
+write.csv(cFu_ZW_20250505_Count, file = "cFu_ZW_20250505_Count.csv", row.names = FALSE)
 
 
 
--- cFu_ZW_20250513_Count.R
+++ cFu_ZW_20250513_Count.R
@@ -23,3 +23,40 @@ cFu_ZW_20250513_Count <- datzw135C %>%
       TRUE ~ NA_real_
     )
   )
+
+
+#Add metadata
+
+cFu_ZW_20250513_Count <- cFu_ZW_20250513_Count %>%
+  mutate(
+    Experiment_ID = "zw135",
+    Collected_By = "WZ",
+    Alina_Dilution = "None",
+    Date_Plated = "Tuesday, May 13, 2025",
+    Date_Read = "Monday, June 2, 2025",
+    MTB_Strain_Type = "Rv/pMv261-zsGreen",
+    Strain_Modifications = NA,
+    Infection_Route = "Aerosol",
+    Mouse_Type = "B6, Ifnar1-/-",
+    Treatment = NA,
+    Organ_Tissue = "Spleen",
+    Sample_Storage = "Frozen after CFU",
+    Preprocessing = "Mash in 6mL PBST",
+    Tissue_Homogenization_Method = "Mash in 6mL PBST",
+    Timepoint_dpi = 28,
+    Log_Dilution_Factor = "-1, -2, -3, -4",
+    Media = "7H11",
+    Media_Additive = "none",
+    Plate_Type = "12 well",
+    Spreading_Method = NA,
+    Total_Plates = NA,
+    Number_of_Replicates = 3,
+    Volume_Per_Plate_uL = 50
+  )
+
+
+
+write.csv(cFu_ZW_20250513_Count, file = "cFu_ZW_20250513_Count.csv", row.names = FALSE)
+
+
+
